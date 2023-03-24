30bca2f0c1e42cb3812884ad45b1d0954255245c
Gareth Bird <garethbird@gmail.com>
2023-03-24 23:06:55 +0000

    tests

--- publish_nuget.bat
+++ publish_nuget.bat
@@ -3,5 +3,4 @@ echo %GITHUB_APIKEY%
 echo %GITHUB_INDEX%
 
 dotnet build --configuration Release
-pause
-dotnet nuget push JpgMetadataEditor/bin/Release/GradientSoftware.Metadata.0.1.0.nupkg --api-key %GITHUB_APIKEY% --source "github" 
+dotnet nuget push Metadata/bin/Release/GradientSoftware.Metadata.0.1.1.nupkg --api-key %GITHUB_APIKEY% --source "github" 
