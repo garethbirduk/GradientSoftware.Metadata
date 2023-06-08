aeeae9efb2d6d940fd80347e04f896768538b9e1
garethbirduk <garethbird@gmail.com>
2023-06-08 09:31:18 +0100

    publish nuget (#7)

--- publish_nuget.bat
+++ publish_nuget.bat
@@ -3,4 +3,4 @@ echo %GITHUB_APIKEY%
 echo %GITHUB_INDEX%
 
 dotnet build --configuration Release
-dotnet nuget push Metadata/bin/Release/GradientSoftware.Metadata.0.1.1.nupkg --api-key %GITHUB_APIKEY% --source "github" 
+dotnet nuget push Metadata/bin/Release/GradientSoftware.Metadata.0.1.2.nupkg --api-key %GITHUB_APIKEY% --source "github" 
